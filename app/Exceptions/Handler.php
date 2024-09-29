48de4d7d8f6e75b38a53bdcf1a50c35b5044beb5
moonn85 <tuanvuanh55@gmail.com>
2024-09-30 00:36:03 +0700

    up

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function report(\Throwable $e)
+{
+    parent::report($e);  // Gọi phương thức cha để đảm bảo tính năng gốc của Laravel được duy trì
+}
+
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, \Throwable $e)
+{
+    return parent::render($request, $e);  // Gọi phương thức cha để xử lý theo cách mặc định của Laravel
+}
+
+}
